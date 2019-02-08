fd6469f9f2781d83a5ac2d68b1605e0442ab3808
Gordon Blasco <gordonblasco@gmail.com>
2019-02-08 14:58:27 -0800

    Got frequency plot working in app. Made a tiny dataset for the app to work on.

--- Seagull_Shiny/app.R
+++ Seagull_Shiny/app.R
@@ -1,49 +1,107 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(shinythemes)
+library(leaflet)
+library(lubridate)
+library(scales)
 
+# Read in data
+gulls <- read_csv("seagulls_small.csv")
+
+####UI####
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  theme = shinytheme("flatly"),
+  
+  # Application title
+  titlePanel("I don't do gulls..."),
+  
+  navbarPage("",
+             
+             tabPanel("Information",
+                      h1("A header!"),
+                      h2("Gull information "),
+                      h2("The tabs"),
+                      p("The frequency plot will tell you the probability of finding a species given the parameters you set"),
+                      p("The interactive map will allow you to see temportal and spacial changes for each species"),
+                      p("Followed by another paragraph of text..."),
+                      h1("Then another header"),
+                      p("You get the idea...)")
+                      
+             ),
+        
+             
+             tabPanel("Frequency Plot", # Tester
+                      
+                      # Sidebar with a slider input for number of bins 
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("county", 
+                                      "Select County",
+                                      choices = c("Santa Barbara",
+                                                  "Los Angeles",
+                                                  "Orange"))
+                      
+                        ),
+                        
+                        # Show a map of selected area 
+                        mainPanel(
+                          plotOutput("FreqPlot")
+                        )
+                      ))
+             
+  )
+  
 )
 
+
+
+
+
+
+
+
+####SERVER####
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+  
+gulls_final <- reactive({ 
+  
+  gulls %>% 
+    filter(
+      county == input$county
+    )  %>% 
+    group_by(common_name) %>% 
+    summarize(
+      n = sum(observation_count)
+    ) %>% 
+    mutate(
+      total = sum(n),
+      prop = n / total
+    ) %>% 
+    arrange(-prop) %>% 
+    mutate(common_name = factor(common_name, levels = common_name))
+  
+  })
+  
+#gills <- data.frame(gulls_final)
+
+output$FreqPlot <- renderPlot({
+ 
+  ggplot(data = gulls_final(), aes(x = common_name, y = prop)) +
+    geom_col() +
+    scale_x_discrete(expand = c(0,0)) +
+    scale_y_continuous(labels=percent_format(),
+                       expand = c(0,0)) +
+    theme_bw() +
+    coord_flip()
+  
+})
+    
+    
+  }
+
+  
 
 # Run the application 
 shinyApp(ui = ui, server = server)
